0ec839144f22486edd5e6bfe5fcbafa4397d22de
ZeGmX <lendy.mulot@gmail.com>
2020-10-19 00:02:09 +0200

    Completed the compiler to Python

--- initial_py_file.py
+++ initial_py_file.py
@@ -1,5 +1,14 @@
+"""
+classification
+"""
+
 import numpy as np
-import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_predict
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, recall_score, f1_score
+
 
 class DSL_Classifier:
     """
@@ -36,7 +45,6 @@ class DSL_Classifier:
         self.use_column = set()
         self.dataset = np.array(())
 
-
     def read(self, path, sep=","):
         """
         Loads the CSV file into the dataset field
@@ -48,43 +56,38 @@ class DSL_Classifier:
         output:
             None
         """
-        self.dataset = np.genfromtxt(path, delimiter=sep, names=True)
-        self.use_all_columns_but_one = set(range(len(self.dataset) - 1))
+        self.dataset = np.genfromtxt(path, delimiter=sep, skip_header=1)
         # -1 because one column (the last one by default) is predicted
-        self.predict_column = len(self.dataset) - 1
+        self.predict_column = len(self.dataset[0]) - 1
+        self.use_column = set(range(len(self.dataset[0]) - 1))
 
-
-    def add_columns(self, l):
+    def add_columns(self, cols):
         """
         Adds predictive columns
         --
         input:
-            l: int list -> the indices of the columns we want to add
+            cols: int list -> the indices of the columns we want to add
         --
         output:
             None
         """
-        for index in l:
-            assert 0 <= index < len(self.dataset[0]), f"Index out of range, \
-got {str(index)} for a dataset of size {len(self.dataset[0])}"
+        for index in cols:
+            assert 0 <= index < len(self.dataset[0]), f"Index out of range, got {str(index)} for a dataset of size {len(self.dataset[0])}"
             self.use_column.add(index)
 
-
-    def remove_columns(self, l):
+    def remove_columns(self, cols):
         """
         Remove predictive columns
         --
         input:
-            l: int list -> the indices of the columns we want to remove
+            cols: int list -> the indices of the columns we want to remove
         --
         output:
             None
         """
-        for index in l:
-            assert 0 <= index < len(self.dataset[0]), f"Index out of range, got \
-            {str(index)} for a dataset of size {len(self.dataset[0])}"
+        for index in cols:
+            assert 0 <= index < len(self.dataset[0]), f"Index out of range, got {str(index)} for a dataset of size {len(self.dataset[0])}"
             self.use_column.discard(index)
-
 
     def predict(self):
         """
@@ -97,13 +100,51 @@ got {str(index)} for a dataset of size {len(self.dataset[0])}"
         output:
             None
         """
-        assert 0 <= self.predict_column < len(self.dataset[0]), f"Index out of \
-range, got predictive index {self.predict_column} for a size \
-{len(self.dataset[0])} dataset"
-        print("hey")
-        # TODO
+        assert 0 <= self.predict_column < len(self.dataset[0]), f"Index out of range, got predictive index {self.predict_column} for a size {len(self.dataset[0])} dataset"
 
+        classifiers = { "svm": SVC(kernel="linear", C=0.025),
+                        "tree": DecisionTreeClassifier(max_depth=5)}
 
+        metrics = { "accuracy": accuracy_score,
+                    "recall": recall_score,
+                    "f1": f1_score}
+
+        # Setting the classifier according to the algorithm
+        if self.algo in classifiers:
+            clf = classifiers[self.algo]
+        else:
+            raise ValueError("algo must be either \"svm\" or \"tree\"")
+
+        # Setting the metric
+        if self.metric in metrics:
+            metric = metrics[self.metric]
+        else:
+            raise ValueError("metric shoud be either \"accuracy\", \"recall\" or \"f1\"")
+
+        expected = self.dataset[:, self.predict_column]
+        others = np.zeros(len(self.dataset[0]), dtype=np.bool)
+        others[list(self.use_column)] = True
+        data = self.dataset[:, others]
+
+        if self.strategy == "train_test":
+            assert 0 <= self.train_test_ratio <= 1, f"Train/test ratio should be between 0 and 1, got {self.train_test_ratio}"
+
+            X_train, X_test, y_train, expected = train_test_split(data, expected,
+                                              train_size=self.train_test_ratio)
+            clf.fit(X_train, y_train)
+            predicted = clf.predict(X_test)
+
+        elif self.strategy == "cross_valid":
+            assert self.cross_valid_nb > 1, f"The number of folds in cross-validation should be greater or equal to 2, got {self.cross_valid_nb}"
+
+            predicted = cross_val_predict(clf, data, y=expected,
+                                          cv=self.cross_valid_nb)
+
+        else:
+            raise ValueError("strategy must be either \"train_test\" or \"cross_valid\"")
+
+        res = metric(expected, predicted)
+        print(f"For the {self.algo} algorithm, and the {self.strategy} strategy, we found a {self.metric}-score of {res}")
 
 
 classifier = DSL_Classifier()
