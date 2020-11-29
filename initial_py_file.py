b0f1346c5a11ba16dc5cf9beb491ccf4fd81c522
ZeGmX <lendy.mulot@gmail.com>
2020-11-29 23:33:22 +0100

    Fixed typo

--- initial_py_file.py
+++ initial_py_file.py
@@ -106,8 +106,8 @@ class DSL_Classifier:
                         "tree": DecisionTreeClassifier(max_depth=5)}
 
         metrics = { "accuracy": accuracy_score,
-                    "recall": recall_score,
-                    "f1": f1_score}
+                    "recall": lambda x, y: recall_score(x, y, average="micro"),
+                    "f1": lambda x, y: f1_score(x, y, average="micro")}
 
         # Setting the classifier according to the algorithm
         if self.algo in classifiers:
@@ -148,3 +148,72 @@ class DSL_Classifier:
 
 
 classifier = DSL_Classifier()
+
+
+
+
+
+"""
+package ml.classification;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import ml.classification.dSL.ML;
+import ml.classification.generator.Compiler2Python;
+import ml.classification.generator.Compiler2R;
+import ml.classification.generator.Interpreter;
+import ml.classification.generator.PrettyPrinter;
+import org.eclipse.xtext.testing.util.ParseHelper;
+
+
+public class Main {
+	static ParseHelper<ML> parseHelper = new parseHelper<ML>();
+
+	public static void main(String[] args)  {
+		if (args.length >= 1) {
+			String path = args[0];
+
+			String content = Files.readString(Paths.get(path));
+			System.out.println(content);
+			ML input = parseHelper.parse(content);
+			assert(input != null);
+
+			PrettyPrinter pp = new PrettyPrinter();
+			System.out.println("****************");
+			System.out.println("* Pretty print *");
+			System.out.println("****************");
+			System.out.println(pp.prettyprint(input));
+			System.out.println("\n\n\n\n\n");
+
+			Interpreter interpreter = new Interpreter();
+			System.out.println("***************");
+			System.out.println("* Interpreter *");
+			System.out.println("***************");
+			interpreter.interpret(input);
+
+			Compiler2Python compPy = new Compiler2Python();
+			System.out.println("***********************");
+			System.out.println("* Generatong .py file *");
+			System.out.println("***********************");
+			String py = compPy.compile(input);
+			String fileNamePy = path.replaceAll(".mldsl", ".py");
+			RandomAccessFile writer = new RandomAccessFile(fileNamePy, "w");
+			writer.writeChars(py);
+			writer.close();
+
+			Compiler2R compR = new Compiler2R();
+			System.out.println("**********************");
+			System.out.println("* Generatong .r file *");
+			System.out.println("**********************");
+			String r = compR.compile(input);
+			String fileNameR = path.replaceAll(".mldsl", ".r");
+			RandomAccessFile writer2 = new RandomAccessFile(fileNameR, "w");
+			writer2.writeChars(r);
+			writer2.close();
+		}
+	}
+}
+"""
